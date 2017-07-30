01c6c285b9d5ab05f1162aec9c4b561188d2e755
Nicolai <nicolaiv@uoc.edu>
2017-07-30 15:33:23 +0300

    Second chapter test

--- src/AppBundle/Controller/SiteController.php
+++ src/AppBundle/Controller/SiteController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle;
+use Doctrine\Common\Cache\Cache;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,6 +15,16 @@ class SiteController extends Controller
      */
     public function aboutAction($name)
     {
+        /** @var Cache $cache */
+        $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
+        $key = md5($name);
+
+        if ($cache->contains($key)) {
+            $name = $cache->fetch($key);
+        } else {
+            $cache->save($key, $name . '_cached');
+        }
+
         return $this->render('site/about.html.twig', [
             'name' => $name,
         ]);
